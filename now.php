04b8b5f7108fbb61d40ba41e6b303971e9b3dcae
Alexandre LE PAPE <alex@alepape.net>
2024-04-07 12:38:50 +0200

    remove debug (too big for header)

--- now.php
+++ now.php
@@ -63,7 +63,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 curl_close($curl);
 $obj = json_decode($response, true);
-header('ALP-debug: '.$response);
+//header('ALP-debug: '.$response);
 
 // result in array
 if (isset($configobj['resultArray'])) {
