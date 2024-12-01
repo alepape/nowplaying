cd24103c41ac2f0a628eae9d7b92a06fefdbade7
Alexandre LE PAPE <alex@alepape.net>
2024-12-01 15:17:47 +0100

    update HA only from html

--- now.php
+++ now.php
@@ -126,25 +126,25 @@ if (isset($configobj['transform'])) {
 	}
 }
 
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'http://rpinet:8123/api/webhook/update-radio-media-jk88hlBW3PeOgtzzDMiCoA-t',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-
 if ($mode == "page") {
 
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => 'http://rpinet:8123/api/webhook/update-radio-media-jk88hlBW3PeOgtzzDMiCoA-t',
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'POST',
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+		
 ?>
 <header>
 <meta http-equiv="refresh" content="30" />
