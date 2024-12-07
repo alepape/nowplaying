2e6cd759938187a6aed166de1b6455ad4cbeff64
Alexandre LE PAPE <alex@alepape.net>
2024-12-07 16:32:29 +0100

    fix default to not loose now.json

--- default.php
+++ default.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // FUNCTIONS
@@ -18,8 +18,13 @@ if ($config == "") {
 }
 
 $configfile = __DIR__ .'/now.json';
+$configjson = file_get_contents($configfile); // reading first
+$configobj = json_decode($configjson, true);
 
 $configdata = ["default" => $config];
+if ($configobj["notification"] != "") { // notif URL - let's not lose it
+    $configdata["notification"] = $configobj["notification"];
+}
 
 file_put_contents($configfile, json_encode($configdata, JSON_PRETTY_PRINT)); 
 // TODO: have a permission check for config json...
