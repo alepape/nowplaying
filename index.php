7d24a07aef761840cbe889efd8e3b37947073987
Alexandre LE PAPE <alex@alepape.net>
2024-12-14 18:11:52 +0100

    desperate attempt at favicon

--- index.php
+++ index.php
@@ -8,11 +8,14 @@ include 'config.php';
 
 ?>
 <header>
-<!-- <meta http-equiv="refresh" content="30" /> -->
+<link rel="apple-touch-icon" sizes="180x180" href="picts/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="picts/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="picts/favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
 <script src="logic.js"></script>
-<link rel="icon" href="picts/favicon.ico" />
 
 </header>
 <body>
