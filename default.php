79aed3e785c891be14d632810759af67fb21d6de
Alexandre LE PAPE <alex@alepape.net>
2024-12-15 18:27:12 +0100

    fix config bug

--- default.php
+++ default.php
@@ -21,12 +21,9 @@ $configfile = __DIR__ .'/now.json';
 $configjson = file_get_contents($configfile); // reading first
 $configobj = json_decode($configjson, true);
 
-$configdata = ["default" => $config];
-if ($configobj["notification"] != "") { // notif URL - let's not lose it
-    $configdata["notification"] = $configobj["notification"];
-}
+$configobj["default"] = $config;
 
-file_put_contents($configfile, json_encode($configdata, JSON_PRETTY_PRINT)); 
+file_put_contents($configfile, json_encode($configobj, JSON_PRETTY_PRINT)); 
 // TODO: have a permission check for config json...
 
 ?>
