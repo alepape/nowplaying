bf8b5988cd2bada3454b3bb89a58cbc6e5d9606c
alepape <alex@alepape.net>
2026-01-18 09:24:08 +0100

    debug for off config

--- config.php
+++ config.php
@@ -26,6 +26,7 @@ if (isset($mainconfigobj['force_ext_path'])) {
 
 $configfile = __DIR__ .'/'.$config.'.json';
 $configjson = file_get_contents($configfile);
+header("ALP-debug: loaded config = ".$configjson);
 $configobj = json_decode($configjson, true);
 // TODO: cache in session?
 
